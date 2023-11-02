19290098dc47b4be881162fef94eb480bcf8aa9c
POLAROID86 <123101498+POLAROID86@users.noreply.github.com>
2023-11-03 01:40:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
 const appRoute = require("./routes/route.js");
+const cors = require("cors");
 
 //Config
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-
+app.use(cors({
+  origin:["https://printcafe1-ndx9.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+}))
 /** routes */
 app.use("/api", appRoute);
 
